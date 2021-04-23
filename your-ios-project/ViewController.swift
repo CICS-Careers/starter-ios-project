1705a11296b0f9e245f421121c26f5bb46d4fe44
Michael Pavkovic <mpavkovic688@gmail.com>
2021-04-23 15:24:13 -0400

    Add counter button

--- your-ios-project/ViewController.swift
+++ your-ios-project/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var count = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBAction func didPressIncrementButton(_ sender: UIButton) {
+        count += 1
+        
+        counterLabel.text = String(count)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        counterLabel.text = String(count)
     }
-
 
 }
 
